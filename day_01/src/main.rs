c660ff15394a409a4eb905e47b9f72b963478f03
Matt Mullins <mmullins@mmlx.us>
2025-11-30 23:22:54 -0800

    day 01: cargo fmt

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,15 +1,19 @@
 static INPUT: &str = include_str!("input.txt");
 
+
 fn main() {
-    let input = INPUT.lines().map(|l| {
-        if let Some(s) = l.strip_prefix("L") {
-            -1 * s.parse::<i32>().expect("input must be integers")
-        } else if let Some(s) = l.strip_prefix("R") {
-            s.parse::<i32>().expect("input must be integers")
-        } else {
-            panic!("input must start with L or R")
-        }
-    }).collect::<Vec<_>>();
+    let input = INPUT
+        .lines()
+        .map(|l| {
+            if let Some(s) = l.strip_prefix("L") {
+                -1 * s.parse::<i32>().expect("input must be integers")
+            } else if let Some(s) = l.strip_prefix("R") {
+                s.parse::<i32>().expect("input must be integers")
+            } else {
+                panic!("input must start with L or R")
+            }
+        })
+        .collect::<Vec<_>>();
 
     let mut count_of_zero = 0;
     let mut position = 50;
