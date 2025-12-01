40b2052b21ae5500043b8fc028bf57f1d170a3d3
Matt Mullins <mmullins@mmlx.us>
2025-11-30 22:53:09 -0800

    day 01: implement part 1
    This is really naive and I'm going to need to actually restructure my
    implementations later, but this works for now.

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,27 @@
+static INPUT: &str = include_str!("input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let input = INPUT.lines().map(|l| {
+        if let Some(s) = l.strip_prefix("L") {
+            -1 * s.parse::<i32>().expect("input must be integers")
+        } else if let Some(s) = l.strip_prefix("R") {
+            s.parse::<i32>().expect("input must be integers")
+        } else {
+            panic!("input must start with L or R")
+        }
+    }).collect::<Vec<_>>();
+
+    let mut count_of_zero = 0;
+    let mut position = 50;
+
+    for &i in &input {
+        position += i;
+        position %= 100;
+
+        if position == 0 {
+            count_of_zero += 1;
+        }
+    }
+
+    println!("part 1: {count_of_zero}");
 }
