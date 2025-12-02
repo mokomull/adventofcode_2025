1eccefa5ade633e9b1fb6e2ced347142357fb961
Matt Mullins <mmullins@mmlx.us>
2025-12-01 21:05:24 -0800

    day 2: parser

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,5 +1,18 @@
+use std::ops::RangeInclusive;
+
 static INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("Hello, world!");
+    let input = parse_input(INPUT);
+}
+
+fn parse_input(input: &str) -> Vec<RangeInclusive<u64>> {
+    input
+        .trim()
+        .split(',')
+        .map(|i| {
+            let [low, high] = i.split('-').collect::<Vec<_>>().try_into().unwrap();
+            (low.parse().unwrap())..=(high.parse().unwrap())
+        })
+        .collect()
 }
