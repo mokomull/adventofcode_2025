e1844cb5d50d6492f5745b9daf89bfd3d2a055f1
Matt Mullins <mmullins@mmlx.us>
2025-12-01 21:10:41 -0800

    day 2: do part 1 naively
    Since it's just "the same string twice" for an invalid ID, it's a pretty simple
    check.  Hopefully my input is small enough this is fine.

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -4,6 +4,8 @@ static INPUT: &str = include_str!("input.txt");
 
 fn main() {
     let input = parse_input(INPUT);
+
+    println!("part 1: {}", part_1(&input));
 }
 
 fn parse_input(input: &str) -> Vec<RangeInclusive<u64>> {
@@ -15,4 +17,22 @@ fn parse_input(input: &str) -> Vec<RangeInclusive<u64>> {
             (low.parse().unwrap())..=(high.parse().unwrap())
         })
         .collect()
+}
+
+fn is_duplicated(id: u64) -> bool {
+    let id = id.to_string();
+    if id.len() % 2 != 0 {
+        return false;
+    }
+
+    id[0..(id.len() / 2)] == id[(id.len() / 2)..]
+}
+
+fn part_1(input: &[RangeInclusive<u64>]) -> u64 {
+    input
+        .iter()
+        .cloned()
+        .flatten()
+        .filter(|&i| is_duplicated(i))
+        .sum()
 }
