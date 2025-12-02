4d211801fa5cc84827ce29e3b096342bcc0550c2
Matt Mullins <mmullins@mmlx.us>
2025-12-01 21:17:33 -0800

    day 2: detect repeats naively as well
    This just breaks the id string into as many different chunk sizes as fit
    evenly, and checks them all.

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let input = parse_input(INPUT);
 
     println!("part 1: {}", part_1(&input));
+    println!("part 2: {}", part_2(&input));
 }
 
 fn parse_input(input: &str) -> Vec<RangeInclusive<u64>> {
@@ -34,5 +35,35 @@ fn part_1(input: &[RangeInclusive<u64>]) -> u64 {
         .cloned()
         .flatten()
         .filter(|&i| is_duplicated(i))
+        .sum()
+}
+
+fn is_repeated(id: u64) -> bool {
+    let id = id.to_string();
+    let id = id.as_bytes();
+
+    for how_many_repeats in 2..id.len() {
+        if id.len() % how_many_repeats != 0 {
+            // if we can't divide the string evenly then there's simply no hope
+            continue;
+        }
+
+        let chunks = id
+            .chunks(id.len() / how_many_repeats)
+            .collect::<Vec<&[u8]>>();
+        if chunks[1..].iter().all(|&chunk| chunk == chunks[0]) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn part_2(input: &[RangeInclusive<u64>]) -> u64 {
+    input
+        .iter()
+        .cloned()
+        .flatten()
+        .filter(|&i| is_repeated(i))
         .sum()
 }
