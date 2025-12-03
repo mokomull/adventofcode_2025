c9451b8a5262d8306fac35b814b09d5600489cf6
Matt Mullins <mmullins@mmlx.us>
2025-12-02 21:30:30 -0800

    day 3: completely redo the joltage algorithm
    Now it breaks it into subproblems instead of blindly iterating all
    combinations.

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,5 +1,3 @@
-use itertools::Itertools;
-
 static INPUT: &str = include_str!("input.txt");
 
 fn main() {
@@ -7,21 +5,40 @@ fn main() {
     println!("part 2: {}", part_2(INPUT.lines()));
 }
 
+fn do_joltage(n: usize, batteries: &[u8]) -> Option<u64> {
+    if n == 0 {
+        return Some(0);
+    }
+
+    if n > batteries.len() {
+        return None;
+    }
+
+    let (next_batteries, this_battery) = batteries.split_at(batteries.len() - 1);
+    assert_eq!(1, this_battery.len());
+    let this_battery = str::from_utf8(&[this_battery[0]])
+        .expect("a digit should be UTF-8")
+        .parse::<u64>()
+        .expect("a digit should be an integer");
+
+    let subproblems = [
+        do_joltage(n, next_batteries),
+        do_joltage(n - 1, next_batteries).map(|j| j * 10 + this_battery),
+    ];
+
+    // Yes, this doesn't need to be unwrapped and then rewrapped, but I'd rather have the diagnostic
+    // if it violates my assumption somewhere deep in the recursion tree.
+    Some(
+        subproblems
+            .into_iter()
+            .flat_map(Option::into_iter)
+            .max()
+            .expect("at least one of them should be Some"),
+    )
+}
+
 fn joltage(batteries: &str) -> u64 {
-    batteries
-        .as_bytes()
-        .iter()
-        .tuple_combinations()
-        .map(|(&a, &b)| {
-            let number = [a, b];
-            let number =
-                str::from_utf8(&number).expect("two digits should still be a valid UTF-8 string");
-            number
-                .parse::<u64>()
-                .expect("two digits should still be an integer")
-        })
-        .max()
-        .unwrap()
+    do_joltage(2, batteries.as_bytes()).expect("input should be longer than 2 to make this work")
 }
 
 fn part_1<'a>(input: impl Iterator<Item = &'a str>) -> u64 {
@@ -29,20 +46,7 @@ fn part_1<'a>(input: impl Iterator<Item = &'a str>) -> u64 {
 }
 
 fn joltage_override(batteries: &str) -> u64 {
-    batteries
-        .as_bytes()
-        .iter()
-        .tuple_combinations()
-        .map(|(&a, &b, &c, &d, &e, &f, &g, &h, &i, &j, &k, &l)| {
-            let number = [a, b, c, d, e, f, g, h, i, j, k, l];
-            let number =
-                str::from_utf8(&number).expect("two digits should still be a valid UTF-8 string");
-            number
-                .parse::<u64>()
-                .expect("two digits should still be an integer")
-        })
-        .max()
-        .unwrap()
+    do_joltage(12, batteries.as_bytes()).expect("input should be longer than 12 to make this work")
 }
 
 fn part_2<'a>(input: impl Iterator<Item = &'a str>) -> u64 {
