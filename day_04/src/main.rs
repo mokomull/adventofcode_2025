4e26b7be923e191fba67325ed6f65d8d3936fe01
Matt Mullins <mmullins@mmlx.us>
2025-12-03 22:17:02 -0800

    day 04: parser

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -1,5 +1,20 @@
 static INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("Hello, world!");
+    let input = parse_input(INPUT);
+}
+
+fn parse_input(input: &str) -> Vec<Vec<bool>> {
+    input
+        .lines()
+        .map(|line| {
+            line.bytes()
+                .map(|b| match b {
+                    b'@' => true,
+                    b'.' => false,
+                    _ => panic!("invalid"),
+                })
+                .collect()
+        })
+        .collect()
 }
