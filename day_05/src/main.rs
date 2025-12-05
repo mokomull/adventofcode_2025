9b3f136b52f026d0a75f04fd32eef2f67fe0b7c0
Matt Mullins <mmullins@mmlx.us>
2025-12-04 21:07:56 -0800

    day 5: parser

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,5 +1,35 @@
+use std::collections::BTreeMap;
+
 static INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("Hello, world!");
+    let (fresh, ingredients) = parse(INPUT);
+}
+
+fn parse(input: &str) -> (BTreeMap<u64, u64>, Vec<u64>) {
+    let mut fresh = BTreeMap::new();
+    let mut ingredients = vec![];
+
+    let mut lines = input.lines();
+    for l in &mut lines {
+        if l.is_empty() {
+            break;
+        }
+
+        let [first, last] = l
+            .split('-')
+            .map(|s| s.parse::<u64>().expect("components must be integers"))
+            .collect::<Vec<_>>()
+            .try_into()
+            .expect("there should be exactly two components to a range");
+
+        fresh.insert(first, last);
+    }
+
+    for l in &mut lines {
+        let i = l.parse().expect("ingredients must be integers");
+        ingredients.push(i);
+    }
+
+    (fresh, ingredients)
 }
