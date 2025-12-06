da1754e91399af1830bce8e22609948b22106505
Matt Mullins <mmullins@mmlx.us>
2025-12-05 22:28:00 -0800

    day 6: parser

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,5 +1,41 @@
+use std::num::ParseIntError;
+
 static INPUT: &str = include_str!("input.txt");
 
+enum Operation {
+    Add,
+    Multiply,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (numbers, operations) = parse(INPUT);
+}
+
+fn parse(input: &str) -> (Vec<Vec<u64>>, Vec<Operation>) {
+    let mut numbers = vec![];
+
+    for line in input.lines() {
+        if let Ok(vs) = line
+            .split(' ')
+            .filter(|word| !word.is_empty())
+            .map(|word| word.parse())
+            .collect::<Result<Vec<u64>, ParseIntError>>()
+        {
+            numbers.push(vs)
+        } else {
+            return (
+                numbers,
+                line.split(' ')
+                    .filter(|word| !word.is_empty())
+                    .map(|word| match word {
+                        "+" => Operation::Add,
+                        "*" => Operation::Multiply,
+                        x => panic!("unexpected word {x:?}"),
+                    })
+                    .collect(),
+            );
+        }
+    }
+
+    panic!("never saw the operations")
 }
