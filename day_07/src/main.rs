73ea375cb78f56288a322d95c52fbd31ac3ea705
Matt Mullins <mmullins@mmlx.us>
2025-12-06 21:17:43 -0800

    day 7: start with parsing

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,5 +1,64 @@
 static INPUT: &str = include_str!("input.txt");
 
+enum Cell {
+    Empty,
+    Splitter,
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn parse(input: &str) -> ((usize, usize), Vec<Vec<Cell>>) {
+    let mut start = None;
+
+    let cells = input
+        .lines()
+        .enumerate()
+        .map(|(i, line)| {
+            line.as_bytes()
+                .iter()
+                .enumerate()
+                .map(|(j, &c)| match c {
+                    b'S' => {
+                        assert!(start.is_none(), "We can't have two starting positions");
+                        start = Some((i, j));
+                        Cell::Empty
+                    }
+                    b'.' => Cell::Empty,
+                    b'^' => Cell::Splitter,
+                    x => panic!("unexpected item {x:?} in the bagging area"),
+                })
+                .collect()
+        })
+        .collect();
+
+    (start.expect("we must have gotten one starting cell"), cells)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse;
+
+    #[test]
+    fn example() {
+        let (start, cells) = parse(
+            ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............",
+        );
+    }
 }
