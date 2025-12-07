0afa8d2fec875eb01bafef2f78d623849a160e07
Matt Mullins <mmullins@mmlx.us>
2025-12-06 21:23:48 -0800

    day 7: implement part 1 naively
    The flavortext was kinda unclear so I made sure to unit-test the example.

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 static INPUT: &str = include_str!("input.txt");
 
 enum Cell {
@@ -36,9 +38,42 @@ fn parse(input: &str) -> ((usize, usize), Vec<Vec<Cell>>) {
     (start.expect("we must have gotten one starting cell"), cells)
 }
 
+fn part_1(start: (usize, usize), cells: &[Vec<Cell>]) -> u64 {
+    let (start_i, start_j) = start;
+
+    let mut splits = 0;
+    let mut beams = HashSet::from([start_j]);
+
+    for line in cells.iter().skip(start_i) {
+        let mut next_beams = HashSet::new();
+
+        for beam in beams {
+            match line[beam] {
+                Cell::Empty => {
+                    next_beams.insert(beam);
+                }
+                Cell::Splitter => {
+                    splits += 1;
+
+                    if beam > 0 {
+                        next_beams.insert(beam - 1);
+                    }
+                    if beam < line.len() - 1 {
+                        next_beams.insert(beam + 1);
+                    }
+                }
+            }
+        }
+
+        beams = next_beams;
+    }
+
+    splits
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::parse;
+    use crate::{parse, part_1};
 
     #[test]
     fn example() {
@@ -60,5 +95,7 @@ mod tests {
 .^.^.^.^.^...^.
 ...............",
         );
+
+        assert_eq!(21, part_1(start, &cells));
     }
 }
