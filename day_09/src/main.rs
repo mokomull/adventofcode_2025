cd12bce0dceecac77c97dfe90fd29cef3cfe430c
Matt Mullins <mmullins@mmlx.us>
2025-12-08 21:04:20 -0800

    day 9: parser

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -1,5 +1,20 @@
 static INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("Hello, world!");
+    let red_squares = parse(INPUT);
+}
+
+fn parse(input: &str) -> Vec<(u32, u32)> {
+    input
+        .lines()
+        .map(|l| {
+            let [a, b] = l
+                .split(',')
+                .map(|s| s.parse().expect("input must be an integer"))
+                .collect::<Vec<_>>()
+                .try_into()
+                .expect("input must be pairs");
+            (a, b)
+        })
+        .collect()
 }
