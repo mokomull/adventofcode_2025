06a46cc024c4796ff84c822c196587f4755684e9
Matt Mullins <mmullins@mmlx.us>
2025-12-08 19:11:18 -0800

    day 8: generate neighborhood ids as we pop edges
    This passes the unit tests for the example, but let's hope it actually runs
    fast enough for personal input...

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -10,6 +10,7 @@ static INPUT: &str = include_str!("input.txt");
 fn main() {
     let boxes = parse(INPUT);
     println!("part 1: {}", part_1(&boxes));
+    println!("part 2: {}", part_2(&boxes));
 }
 
 fn parse(input: &str) -> Vec<(u32, u32, u32)> {
@@ -94,9 +95,80 @@ fn part_1(boxes: &[(u32, u32, u32)]) -> u32 {
     do_connections::<1000>(boxes)
 }
 
+fn part_2(boxes: &[(u32, u32, u32)]) -> u32 {
+    let mut edges = generate_edge_heap(boxes);
+    let mut neighborhood_ids_by_box = HashMap::new();
+
+    // this outer loop variable exists simply to generate new neighborhood ids.  not all will be
+    // used.
+    for neighborhood in 0u32.. {
+        let (_distance, i, j) = edges.pop().expect("we ran out of edges");
+
+        let neighborhood_i = neighborhood_ids_by_box.get(&i);
+        let neighborhood_j = neighborhood_ids_by_box.get(&j);
+
+        // if they've both been assigned a neighborhood id, then we just need to pick a canonical
+        // one and update the whole other subgraph
+        if let Some(&neighborhood_a) = neighborhood_i
+            && let Some(&neighborhood_b) = neighborhood_j
+        {
+            // if they're already in the same neighborhood, we can even skip our "are we done" check
+            // that doesn't actually make anything any more done
+            if neighborhood_a == neighborhood_b {
+                continue;
+            }
+
+            // by fair dice roll, the canonical neighborhood id is the lower id
+            let canonical_id = std::cmp::min(neighborhood_a, neighborhood_b);
+            for v in neighborhood_ids_by_box.values_mut() {
+                if *v == neighborhood_a || *v == neighborhood_b {
+                    *v = canonical_id;
+                }
+            }
+        } else if let Some(&existing_neighborhood) = neighborhood_i {
+            // if i was already in a neighborhood, then j now belongs to it too
+            assert!(
+                neighborhood_ids_by_box
+                    .insert(j, existing_neighborhood)
+                    .is_none(),
+                "oops overwrote a neighborhood"
+            );
+        } else if let Some(&existing_neighborhood) = neighborhood_j {
+            // if j was already in a neighborhood, then i now belongs to it too
+            assert!(
+                neighborhood_ids_by_box
+                    .insert(i, existing_neighborhood)
+                    .is_none(),
+                "oops overwrote a neighborhood"
+            );
+        } else {
+            // and if neither was, then they both belong to a brand new neighborhood now.  This is
+            // the only case that actually uses the outer-loop's loop variable.
+            assert!(
+                neighborhood_ids_by_box.insert(i, neighborhood).is_none(),
+                "oops overwrote a neighborhood"
+            );
+            assert!(
+                neighborhood_ids_by_box.insert(j, neighborhood).is_none(),
+                "oops overwrote a neighborhood"
+            );
+        }
+
+        if neighborhood_ids_by_box.len() == boxes.len()
+            && neighborhood_ids_by_box.values().all_equal()
+        {
+            let (x1, _y1, _z1) = boxes[i];
+            let (x2, _y2, _z2) = boxes[j];
+            return x1 * x2;
+        }
+    }
+
+    panic!("we never finished")
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{INPUT, do_connections, parse, part_1};
+    use crate::{INPUT, do_connections, parse, part_1, part_2};
 
     #[test]
     fn example() {
@@ -122,7 +194,8 @@ mod tests {
 984,92,344
 425,690,689",
         );
-        assert_eq!(40, do_connections::<10>(&boxes))
+        assert_eq!(40, do_connections::<10>(&boxes));
+        assert_eq!(25272, part_2(&boxes));
     }
 
     #[test]
