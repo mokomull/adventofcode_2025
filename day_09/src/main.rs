f04281f0bb168914040a92250bd78c9343546a94
Matt Mullins <mmullins@mmlx.us>
2025-12-08 21:06:50 -0800

    day 9: naive solution
    Just try all pairs of corners.

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -1,7 +1,10 @@
+use itertools::Itertools;
+
 static INPUT: &str = include_str!("input.txt");
 
 fn main() {
     let red_squares = parse(INPUT);
+    println!("part 1: {}", part_1(&red_squares));
 }
 
 fn parse(input: &str) -> Vec<(u32, u32)> {
@@ -17,4 +20,15 @@ fn parse(input: &str) -> Vec<(u32, u32)> {
             (a, b)
         })
         .collect()
+}
+
+fn part_1(red_squares: &[(u32, u32)]) -> u64 {
+    red_squares
+        .iter()
+        .tuple_combinations()
+        .map(|(&(x1, y1), &(x2, y2))| {
+            (x1 as i64 - x2 as i64).unsigned_abs() * (y1 as i64 - y2 as i64).unsigned_abs()
+        })
+        .max()
+        .expect("there was at least one pair of squares")
 }
