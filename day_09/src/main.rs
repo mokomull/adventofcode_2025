64c50651b6bbebee9751e83b965f361f03018d30
Matt Mullins <mmullins@mmlx.us>
2025-12-08 22:04:31 -0800

    day 9: start figuring out if it's in the loop
    This foray through handedness of a corner is demonstrably wrong:
    
        #-------A
        |       |
        |       L----C
        |         @  |
        |            |
        #------------#
    
    The point marked @ is on the "left" side of the A->L line, but the right side
    of the L->C line, even though it is actually inside the polygon.
    
    I'm going to have to actually look up an algorithm for this.

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -5,6 +5,8 @@ use std::{
 
 use itertools::Itertools;
 
+use Handedness::*;
+
 static INPUT: &str = include_str!("input.txt");
 
 fn main() {
@@ -40,23 +42,6 @@ fn part_1(red_squares: &[(u32, u32)]) -> u64 {
 }
 
 fn part_2(red_squares: &[(u32, u32)]) -> u64 {
-    let red_or_green_squares: HashSet<(u32, u32)> = red_squares
-        .iter()
-        .chain([&red_squares[0]].into_iter())
-        .tuple_windows()
-        .flat_map(
-            |(&(x1, y1), &(x2, y2))| -> Box<dyn Iterator<Item = (u32, u32)>> {
-                if x1 == x2 {
-                    Box::new((min(y1, y2)..=max(y1, y2)).map(move |y| (x1, y)))
-                } else if y1 == y2 {
-                    Box::new((min(x1, x2)..=max(x1, x2)).map(move |x| (x, y1)))
-                } else {
-                    panic!("the tiles did not form a straight horizontal or vertical line");
-                }
-            },
-        )
-        .collect();
-
     let mut areas = red_squares
         .iter()
         .tuple_combinations()
@@ -74,19 +59,49 @@ fn part_2(red_squares: &[(u32, u32)]) -> u64 {
     areas
         .into_iter()
         .filter_map(|((x1, y1), (x2, y2), area)| {
-            let xs = min(x1, x2)..=max(x1, x2);
-            let ys = min(y1, y2)..=max(y1, y2);
-            if xs
-                .cartesian_product(ys)
-                .all(|cell| red_or_green_squares.contains(&cell))
+            // For this rectangle to be fully on red-or-green squares:
+            //   * at least one of the corners must be inside the red-or-green polygon (I will chose
+            //     (x1, y1)), and
+            //   * none of the line segments of the loop may pass through the inside of this
+            //     rectangle.  Since the borders of the loop are considered OK, the loop may touch
+            //     the borders of this rectangle -- it is strictly the inside that we care about.
+
+            // Iterate over the corners of the loop.  This is the corner at point `b`, and treating
+            // it for handedness as if it were from `a` via `b` to `c`.
+            for (&a, &b, &c) in red_squares
+                .iter()
+                .chain([&red_squares[0]].into_iter())
+                .tuple_windows()
             {
-                Some(area)
-            } else {
-                None
+                // If (x1, y1) is outside the polygon, then for some corner it will be on different
+                // sides of the a->b line than the b->c line.  I don't actually have a proof for
+                // this, but it feels like the sort of trick that would work.
             }
+
+            Some(area)
         })
         .next()
         .expect("no rectangles that are filled with only red and green")
+}
+
+enum Handedness {
+    OnTheLine,
+    Left,
+    Right,
+}
+
+impl Handedness {
+    fn compute(a: (usize, usize), b: (usize, usize), x: (usize, usize)) -> Handedness {
+        if a.0 == b.0 {
+            if a.1 > b.1 {
+                if x.0 == a.0 && (b.1..=a.1).contains(&x.1) {
+                    return OnTheLine;
+                }
+            }
+        }
+
+        todo!()
+    }
 }
 
 #[cfg(test)]
