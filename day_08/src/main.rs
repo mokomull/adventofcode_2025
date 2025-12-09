04d05cb5ac7090311d6078e56b7429bbab22bde2
Matt Mullins <mmullins@mmlx.us>
2025-12-08 18:09:16 -0800

    day 8: parser

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -1,5 +1,20 @@
 static INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("Hello, world!");
+    let boxes = parse(INPUT);
+}
+
+fn parse(input: &str) -> Vec<(u32, u32, u32)> {
+    input
+        .lines()
+        .map(|line| {
+            let [a, b, c] = line
+                .split(',')
+                .map(|s| s.parse().expect("input must be an integer"))
+                .collect::<Vec<_>>()
+                .try_into()
+                .expect("input must have exactly 3 components");
+            (a, b, c)
+        })
+        .collect()
 }
