960728ce0c194f456b9954baba41e2bbe870e776
Matt Mullins <mmullins@mmlx.us>
2025-12-08 21:32:43 -0800

    day 9: try to naively implement part 2
    I already realize I forgot to deal with the "all tiles inside the loop are also
    green".

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    cmp::{Reverse, max, min},
+    collections::HashSet,
+};
+
 use itertools::Itertools;
 
 static INPUT: &str = include_str!("input.txt");
@@ -34,9 +39,59 @@ fn part_1(red_squares: &[(u32, u32)]) -> u64 {
         .expect("there was at least one pair of squares")
 }
 
+fn part_2(red_squares: &[(u32, u32)]) -> u64 {
+    let red_or_green_squares: HashSet<(u32, u32)> = red_squares
+        .iter()
+        .chain([&red_squares[0]].into_iter())
+        .tuple_windows()
+        .flat_map(
+            |(&(x1, y1), &(x2, y2))| -> Box<dyn Iterator<Item = (u32, u32)>> {
+                if x1 == x2 {
+                    Box::new((min(y1, y2)..=max(y1, y2)).map(move |y| (x1, y)))
+                } else if y1 == y2 {
+                    Box::new((min(x1, x2)..=max(x1, x2)).map(move |x| (x, y1)))
+                } else {
+                    panic!("the tiles did not form a straight horizontal or vertical line");
+                }
+            },
+        )
+        .collect();
+
+    let mut areas = red_squares
+        .iter()
+        .tuple_combinations()
+        .map(|(&(x1, y1), &(x2, y2))| {
+            (
+                (x1, y1),
+                (x2, y2),
+                ((x1 as i64 - x2 as i64).unsigned_abs() + 1)
+                    * ((y1 as i64 - y2 as i64).unsigned_abs() + 1),
+            )
+        })
+        .collect_vec();
+    areas.sort_unstable_by_key(|&(_corner_a, _corner_b, area)| Reverse(area));
+
+    areas
+        .into_iter()
+        .filter_map(|((x1, x2), (y1, y2), area)| {
+            let xs = min(x1, x2)..=max(x1, x2);
+            let ys = min(y1, y2)..=max(y1, y2);
+            if xs
+                .cartesian_product(ys)
+                .all(|cell| red_or_green_squares.contains(&cell))
+            {
+                Some(area)
+            } else {
+                None
+            }
+        })
+        .next()
+        .expect("no rectangles that are filled with only red and green")
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{parse, part_1};
+    use crate::{parse, part_1, part_2};
 
     #[test]
     fn example() {
@@ -51,5 +106,6 @@ mod tests {
 7,3",
         );
         assert_eq!(50, part_1(&red_squares));
+        assert_eq!(24, part_2(&red_squares));
     }
 }
