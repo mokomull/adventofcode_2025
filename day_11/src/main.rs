fe3a051e84e71fbf75cdf0620eb7a34fd19eb765
Matt Mullins <mmullins@mmlx.us>
2025-12-10 22:06:07 -0800

    day 11: parser

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -1,5 +1,32 @@
+use std::collections::HashMap;
+
+use petgraph::{Graph, graph::NodeIndex};
+
 static INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("Hello, world!");
+    let (graph, you, out) = parse(INPUT);
+}
+
+fn parse<'a>(input: &'a str) -> (Graph<(), ()>, NodeIndex, NodeIndex) {
+    let mut nodes = HashMap::new();
+    let mut graph = Graph::new();
+
+    for line in input.lines() {
+        let (from, tos) = line
+            .split_once(": ")
+            .expect("line must contain from and to");
+        let tos = tos.split(' ');
+
+        for node in std::iter::once(from).chain(tos.clone()) {
+            nodes.entry(node).or_insert_with(|| graph.add_node(()));
+        }
+
+        let from = nodes[from];
+        for to in tos {
+            graph.add_edge(from, nodes[to], ());
+        }
+    }
+
+    (graph, nodes["you"], nodes["out"])
 }
