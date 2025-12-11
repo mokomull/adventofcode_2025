11e01e782050c9aff6e5cfdc58bb0b64029947e6
Matt Mullins <mmullins@mmlx.us>
2025-12-10 22:20:48 -0800

    day 11: part 2 paths go from svr to out

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -5,12 +5,21 @@ use petgraph::{Graph, algo::all_simple_paths, graph::NodeIndex};
 static INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    let (graph, you, out, fft, dac) = parse(INPUT);
+    let (graph, you, out, svr, fft, dac) = parse(INPUT);
     println!("part 1: {}", part_1(&graph, you, out));
-    println!("part 2: {}", part_2(&graph, you, out, fft, dac));
+    println!("part 2: {}", part_2(&graph, svr, out, fft, dac));
 }
 
-fn parse<'a>(input: &'a str) -> (Graph<(), ()>, NodeIndex, NodeIndex, NodeIndex, NodeIndex) {
+fn parse<'a>(
+    input: &'a str,
+) -> (
+    Graph<(), ()>,
+    NodeIndex,
+    NodeIndex,
+    NodeIndex,
+    NodeIndex,
+    NodeIndex,
+) {
     let mut nodes = HashMap::new();
     let mut graph = Graph::new();
 
@@ -34,6 +43,7 @@ fn parse<'a>(input: &'a str) -> (Graph<(), ()>, NodeIndex, NodeIndex, NodeIndex,
         graph,
         nodes["you"],
         nodes["out"],
+        nodes["svr"],
         nodes["fft"],
         nodes["dac"],
     )
